c2b96beb63459bec755c9ce03be2ea3e61f1281e
imneli <contatodoneli@gmail.com>
2024-09-23 11:17:15 -0300

    Icons Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,16 @@
+import { Facebook, Instagram, Twitter } from "lucide-react";
+
 function Footer() {
     return(
         <>
         <footer className="mt-16 bg-muted py-8">
-            <div className="container mx-auto text-center">
+            <div className="container mx-auto text-center flex flex-col justify-center">
             <p>&copy; 2024 ElectroDrive. All rights reserved.</p>
-            <p>add icons here</p>
+            <div className="flex m-4 justify-center">
+                <Facebook/>
+                <Twitter className="ms-4 me-4"/>
+                <Instagram/>
+            </div>
             </div>
       </footer>
       </>
