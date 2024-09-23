a7ad4bfbb649db236be789acd8885df3dd873771
imneli <contatodoneli@gmail.com>
2024-09-23 11:34:02 -0300

    Icons certos

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,7 @@
 import { FaFacebookSquare } from "react-icons/fa";
-import { FaXTwitter } from "react-icons/fa6";
 import { FaInstagram } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { HiOutlineMail } from "react-icons/hi";
 
 function Footer() {
     return(
@@ -9,9 +10,10 @@ function Footer() {
             <div className="container mx-auto text-center flex flex-col justify-center">
             <p>&copy; 2024 ElectroDrive. All rights reserved.</p>
             <div className="flex m-4 justify-center">
-                <FaFacebookSquare className="text-3xl" />
-                <FaXTwitter className="ms-4 me-4 text-3xl"/>
-                <FaInstagram className="text-3xl"/>
+                <FaFacebookSquare className=" m-4 text-3xl" />
+                <FaInstagram className=" m-4 text-3xl"/>
+                <FaLinkedin className=" m-4 text-3xl"/>
+                <HiOutlineMail className=" m-4 text-3xl"/>
             </div>
             </div>
       </footer>
